48ed347667e7620439c58418066cd203cbeab540
Drew Johnston <dmj2130@columbia.edu>
2017-01-10 23:22:22 -0500

    added credential features

---
+++ excelmgmt.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import pyexcel
+from webfns import get_credentials
+from oauth2client import tools
+import httplib2
+from apiclient import discovery
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'FullyAutomatedLuxuryBudget'
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+      print('Labels:')
+      for label in labels:
+        print(label['name'])
+
+
+
+
+
+if __name__ == '__main__':
+    main()
