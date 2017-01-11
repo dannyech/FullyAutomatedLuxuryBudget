9240fea1525b2ce081eee80f060331f62319f8ad
Drew Johnston <dmj2130@columbia.edu>
2017-01-11 00:38:02 -0500

    added test

--- excelmgmt.py
+++ excelmgmt.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import pyexcel
+import pyexcel_io
+from pyexcel_io import get_data
 from webfns import get_credentials
 from oauth2client import tools
 import httplib2
@@ -14,21 +16,45 @@ except ImportError:
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'FullyAutomatedLuxuryBudget'
+CSV_FILE = None
 
 def main():
+    global CSV_FILE
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
+    CSV_FILE = sys.argv[1]
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
 
-    if not labels:
-        print('No labels found.')
+def process_email(message)
+    global CSV_FILE
+    data = pyexcel_io.get_data(CSV_FILE)
+    if "," not in message:
+        return
+    split = message.split(",")
+    split[:len(split)-1] = [item.lower() for item in split[:len(split)-1]]
+    if len(split)==3:
+        if "c:" in split[0] and "s:" in split[1]:
+            try:
+                first = int(split[0].split("c:")[0])
+                second = int(split[1].split("s:")[1])
+            except ValueError:
+                return
+
+        elif "s:" in split[0] and "c:" in split[1]:
+
+    elif len(split)==2:
+        if "c:" in split[:0]:
+
+        elif "s:" in split[:0]
+
+        else:
+
     else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
+        return
+
+
+
 
 
 
