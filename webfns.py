fa343e4c36031f7bc814fb251b6705ad3f102308
Drew Johnston <dmj2130@columbia.edu>
2017-01-11 01:35:36 -0500

    merge proofing

--- webfns.py
+++ webfns.py
@@ -1,16 +1,8 @@
 from __future__ import print_function
-import httplib2
 import os
-from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
 def get_credentials():
     home_dir = os.path.expanduser('~')
