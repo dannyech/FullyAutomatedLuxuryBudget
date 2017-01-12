b32dab2596eec782149327a0a8029b8a7b5316fd
Drew Johnston <dmj2130@columbia.edu>
2017-01-11 21:47:41 -0500

    bugfix in webfns

--- webfns.py
+++ webfns.py
@@ -20,5 +20,6 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, None)
         print('Storing credentials to ' + credential_path)
     return credentials
