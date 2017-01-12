61d85f512567d92ecff7fd4a84ecb6233f374f3a
dannyech <dannyech@gmail.com>
2017-01-11 21:34:08 -0500

    made code prettier like drew

--- emailmgmt.py
+++ emailmgmt.py
@@ -8,46 +8,39 @@ from apiclient import discovery, errors
 NEW_MESSAGES = 'is:unread' 
 
 def get_messages(service, user_id):
-    messages = get_newest_messages(service, user_id)
-    message_texts = []
-    for message in messages:
-        message_texts.append(get_message_text(service, user_id, message['id']))
-        mark_message_old(service, user_id, message['id'])
-
-    return message_texts
+    try:
+        messages = get_newest_messages(service, user_id)
+        message_texts = []
+        for message in messages:
+            message_texts.append(get_message_text(service, user_id, message['id']))
+            #mark_message_old(service, user_id, message['id'])
+        return message_texts
+    except errors.HttpError, error:
+        print('An error occurred: %s' % error)
 
 def get_newest_messages(service, user_id):
-    try:
-        response = service.users().messages().list(userId=user_id, 
-            q=NEW_MESSAGES).execute()
+    response = service.users().messages().list(userId=user_id, 
+        q=NEW_MESSAGES).execute()
 
-        if 'messages' in response:
-            messages = response['messages']
-            while 'nextPageToken' in response:
-                page_toke = response['nextPageToken']
-                response = service.users().messages().list(userId=user_id, 
-                    q=NEW_MESSAGES, pageToken=page_token).execute()
-                messages.extend(response['messages'])
-        else:
-            messages = []
+    if 'messages' in response:
+        messages = response['messages']
+        while 'nextPageToken' in response:
+            page_toke = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, 
+                q=NEW_MESSAGES, pageToken=page_token).execute()
+            messages.extend(response['messages'])
+    else:
+        messages = []
 
-        return messages
-    except errors.HttpError, error:
-        print('An error occurred: %s' % error)
+    return messages
 
 def mark_message_old(service, user_id, message_id):
-    try:
-        service.users().messages().modify(userId=user_id, id=message_id, 
-            body={ 'removeLabelIds': ['UNREAD']}).execute()
-    except errors.HttpError, error:
-        print('An error occurred: %s' % error)
+    service.users().messages().modify(userId=user_id, id=message_id, 
+        body={ 'removeLabelIds': ['UNREAD']}).execute()
 
 def get_message_text(service, user_id, message_id):
-  try:
     message = service.users().messages().get(userId=user_id, 
         id=message_id).execute()
     text = message['snippet']
     return text
-  except errors.HttpError, error:
-    print('An error occurred: %s' % error)
 
